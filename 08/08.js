2eeb2367e70581fde3d5942e4f0a78a8596ccff0
victorraphaelmilk <victorraphaelleitemaciel@gmail.com>
2025-04-16 13:45:48 -0300

    08

---
+++ 08/08.js
@@ -0,0 +1,30 @@
+function contarImparesEPares (number) {
+    
+    number = number.toString()
+    
+    var impares = 0
+    var pares = 0
+
+    for(let i = 0; i < number.length; i++) {
+        
+        num = parseInt(number[i])
+
+        if (num % 2 == 0) {
+
+            pares = pares + 1
+        } else {
+
+            impares = impares + 1
+
+
+        }
+        
+        
+        
+    }
+
+    return {pares: pares, impares: impares}
+
+}
+
+console.log(contarImparesEPares(2822))
